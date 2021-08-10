0693ab5660b6cca0744b90ca36ba8cda0a03db7d
Abdullah AlHussein <70335592+AbdullahAlhussein@users.noreply.github.com>
2021-08-10 19:33:27 +0300

    Update EToken_22.java

--- eToken_22/src/EToken_22.java
+++ eToken_22/src/EToken_22.java
@@ -27,8 +27,8 @@ public class EToken_22 {
 
     	
 	    // Create instance of SunPKCS11 provider
-     	String pkcs11Config = "C:\\Users\\ö0570417090\\eclipse-workspace\\EToken_22\\config.cfg";
-    	java.io.ByteArrayInputStream pkcs11ConfigStream = new java.io.ByteArrayInputStream(pkcs11Config.getBytes());
+     	    String pkcs11Config = "C:\\Users\\Ã¶0570417090\\eclipse-workspace\\EToken_22\\config.cfg";
+    	    java.io.ByteArrayInputStream pkcs11ConfigStream = new java.io.ByteArrayInputStream(pkcs11Config.getBytes());
 	    sun.security.pkcs11.SunPKCS11 providerPKCS11 = new sun.security.pkcs11.SunPKCS11(pkcs11Config);
 	    java.security.Security.addProvider(providerPKCS11);   
 	   
